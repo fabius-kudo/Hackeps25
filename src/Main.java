21606bec2b5197cc51f2d6f7a5beb7c5cc1c3d74
César <cesarantonio.r@students.salle.url.edu>
2025-11-22 18:06:58 +0100

    Code to read .json file of the crimes.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+
 public class Main {
-    public void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) {
+
+        ReadCrimes.readCrimes();
+
     }
 
 }

7a1b4c9056184b99a428704ec8e7bd4989264c23
Nói Leistner <noi.leistner@gmail.com>
2025-11-22 13:19:39 +0100

    hello world

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-
+public class Main {
+    public void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 
 }
